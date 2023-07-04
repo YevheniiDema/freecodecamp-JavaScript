4405c9d7143ce479c752eb068e3f44cd078f2903
YevheniiDema <jekdema@gmail.com>
2023-07-04 23:51:30 +0200

    Declare a Read-Only Variable with the const Keyword

--- basic_java_script/Comment Your JavaScript Code.js
+++ basic_java_script/Comment Your JavaScript Code.js
@@ -1,12 +1,21 @@
-// Explore Differences Between the var and let Keywords
-// One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations:
+// Declare a Read-Only Variable with the const Keyword
+// The keyword let is not the only new way to declare variables. In ES6, you can also declare variables using the const keyword.
 //
-//     var camper = "James";
-// var camper = "David";
-// console.log(camper);
-// So unlike var, when you use let, a variable with the same name can only be declared once.
+// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
 //
-//     Update the code so it only uses the let keyword.
+//     const FAV_PET = "Cats";
+// FAV_PET = "Dogs";
+// The console will display an error due to reassigning the value of FAV_PET.
+//
+//     You should always name variables you don't want to reassign using the const keyword. This helps when you accidentally attempt to reassign a variable that is meant to stay constant.
+//
+// Note: It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). You will learn more about objects, arrays, and immutable and mutable values in later challenges. Also in later challenges, you will see examples of uppercase, lowercase, or camelCase variable identifiers.
+//
+//     Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+//
 
-let catName = "Oliver";
-let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
