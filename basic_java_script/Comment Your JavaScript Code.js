ace2464e0b3f8f09a9609db082d5661c5c91df4b
YevheniiDema <jekdema@gmail.com>
2023-07-04 23:38:48 +0200

    Understanding Uninitialized Variables

--- basic_java_script/Comment Your JavaScript Code.js
+++ basic_java_script/Comment Your JavaScript Code.js
@@ -1,20 +1,23 @@
-// Description
+// Understanding Uninitialized Variables
+// When JavaScript variables are declared, they have an initial value of undefined.
+//     If you do a mathematical operation on an undefined variable your result will be NaN which means
+// "Not a Number". If you concatenate a string with an undefined variable, you will get a string of undefined.
 //
-// Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+//     Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will
+// not be undefined.Understanding Uninitialized Variables
+// When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical
+// operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a
+// string with an undefined variable, you will get a string of undefined.
 //
-//     There are two ways to write comments in JavaScript:
-//
-// Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
-//
-// // This is an in-line comment.
-// You can make a multi-line comment beginning with /* and ending with */. This is a multi-line comment:
-//
-//     /* This is a
-//     multi-line comment */
-//     NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code. Good commenting can help communicate the intent of your code—both for others and for your future self.
-//
-//     Try creating one of each type of comment.
+//     Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not
+// be undefined.
 
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
 
-//This is a first comment
-/*This is a second comment*/
+a = a + 1;
+b = b + 5;
+c = c + " String!";
