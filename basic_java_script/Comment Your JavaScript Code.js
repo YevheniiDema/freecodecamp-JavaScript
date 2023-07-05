5547719440cc7422252219f62144f68383865a8c
YevheniiDema <jekdema@gmail.com>
2023-07-05 19:03:39 +0200

    Declare a Read-Only Variable with the const Keyword

--- basic_java_script/Comment Your JavaScript Code.js
+++ basic_java_script/Comment Your JavaScript Code.js
@@ -1,21 +1,15 @@
-// Declare a Read-Only Variable with the const Keyword
-// The keyword let is not the only new way to declare variables. In ES6, you can also declare variables using the const keyword.
+// Add Two Numbers with JavaScript
+//     Number is a data type in JavaScript which represents numeric data.
 //
-// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
+//     Now let's try to add two numbers using JavaScript.
 //
-//     const FAV_PET = "Cats";
-// FAV_PET = "Dogs";
-// The console will display an error due to reassigning the value of FAV_PET.
+// JavaScript uses the + symbol as an addition operator when placed between two numbers.
 //
-//     You should always name variables you don't want to reassign using the const keyword. This helps when you accidentally attempt to reassign a variable that is meant to stay constant.
+//     Example:
 //
-// Note: It is common for developers to use uppercase variable identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). You will learn more about objects, arrays, and immutable and mutable values in later challenges. Also in later challenges, you will see examples of uppercase, lowercase, or camelCase variable identifiers.
+// const myVar = 5 + 10;
+// myVar now has the value 15.
 //
-//     Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
-//
+// // Change the 0 so that sum will equal 20.
 
-
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+const sum = 10 + 10;
