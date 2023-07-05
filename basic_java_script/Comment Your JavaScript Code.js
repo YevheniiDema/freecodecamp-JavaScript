8b4b9db50484e80f8a460371e9273aeb1e3932bb
YevheniiDema <jekdema@gmail.com>
2023-07-05 19:06:10 +0200

    Divide One Number by Another with JavaScript

--- basic_java_script/Comment Your JavaScript Code.js
+++ basic_java_script/Comment Your JavaScript Code.js
@@ -1,15 +1,13 @@
-// Add Two Numbers with JavaScript
-//     Number is a data type in JavaScript which represents numeric data.
+// Divide One Number by Another with JavaScript
+//     We can also divide one number by another.
 //
-//     Now let's try to add two numbers using JavaScript.
+//     JavaScript uses the / symbol for division.
 //
-// JavaScript uses the + symbol as an addition operator when placed between two numbers.
+//                                          Example
 //
-//     Example:
+//                                      const myVar = 16 / 2;
+// myVar now has the value 8.
 //
-// const myVar = 5 + 10;
-// myVar now has the value 15.
-//
-// // Change the 0 so that sum will equal 20.
+// Change the 0 so that the quotient is equal to 2.
 
-const sum = 10 + 10;
+const quotient = 66 / 33;
