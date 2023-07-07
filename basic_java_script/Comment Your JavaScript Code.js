2448c0fd4c75abb77a0e02b94ee77c769a2a6f0f
YevheniiDema <jekdema@gmail.com>
2023-07-07 21:14:25 +0200

    Bracket notation

--- basic_java_script/Comment Your JavaScript Code.js
+++ basic_java_script/Comment Your JavaScript Code.js
@@ -1,16 +1,23 @@
-// Appending Variables to Strings
-// Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+// Use Bracket Notation to Find the First Character in a StringBracket notation is a way to get a character at a specific index within a string.
+//
+//     Most modern programming languages, like JavaScript, don't start counting at 1 like humans do. They start at 0. This is referred to as Zero-based indexing.
+//
+// For example, the character at index 0 in the word Charles is C. So if const firstName = "Charles", you can get the value of the first letter of the string by using firstName[0].
 //
 //     Example:
 //
-// const anAdjective = "awesome!";
-// let ourStr = "freeCodeCamp is ";
-// ourStr += anAdjective;
-// ourStr would have the value freeCodeCamp is awesome!.
+// const firstName = "Charles";
+// const firstLetter = firstName[0];
+// firstLetter would have a value of the string C.
 //
-// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+//     Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+//
+//     Hint: Try looking at the example above if you get stuck.
+//
+// /
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
 
-// Change code below this line
-const someAdjective = "great";
-let myStr = "Learning to code is ";
-myStr += someAdjective;
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
