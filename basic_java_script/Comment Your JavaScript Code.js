e24f90a9dfbec0aaa89f8c931ffffcbeda573d47
YevheniiDema <jekdema@gmail.com>
2023-07-14 09:36:09 +0200

    Local Scope and Functions

--- basic_java_script/Comment Your JavaScript Code.js
+++ basic_java_script/Comment Your JavaScript Code.js
@@ -1,25 +1,10 @@
-// Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
-//
-// Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
-
-// Declare the myGlobal variable below this line
-let myGlobal = 10;
-
-
-function fun1() {
-    // Assign 5 to oopsGlobal here
-    oopsGlobal = 5;
+function myLocalScope() {
+    // Only change code below this line
+    let myVar = 5;
+    console.log('inside myLocalScope', myVar);
 }
+myLocalScope();
 
-// Only change code above this line
-
-function fun2() {
-    let output = "";
-    if (typeof myGlobal != "undefined") {
-        output += "myGlobal: " + myGlobal;
-    }
-    if (typeof oopsGlobal != "undefined") {
-        output += " oopsGlobal: " + oopsGlobal;
-    }
-    console.log(output);
-}
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
